3d0f28087954f702ff3f1d05999a9e3d6598c070
Oleksii Kushch <oleksii.kushch@nure.ua>
2023-06-26 00:02:00 +0300

    Update README file and remove all unnecessary files.

--- target/FinalProjUltimate/resources/script/js/custom/reject.product.js
+++
@@ -1,5 +0,0 @@
-$(".rejectSwitcher").click(function () {
-    let id = $(this).val();
-    document.getElementById("rejectInputAmount" + id).disabled = !$(this).prop('checked');
-    checkAllRejectSwitchers();
-});
